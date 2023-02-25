5b41f38f5a507d5fef1e82d304589839dd6b465a
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-02-25 10:56:40 -0300

    adding routes and http methods

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
+
+  response.json({ name, email, password });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
