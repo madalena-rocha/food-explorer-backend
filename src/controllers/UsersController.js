2ff261651d0f1fa695f3fba80f970a7eaf89d77e
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-03-01 11:47:19 -0300

    registering user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,26 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body;
+  async create(request, response) {
+    const { name, email, password, is_admin = false } = request.body;
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório!");
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    response.status(201).json({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)",
+      [name, email, hashedPassword, is_admin]
+    );
+
+    return response.status(201).json();
   }
 }
 
