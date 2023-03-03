756645376d1c5d4cf80072fa15253da939294243
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-03-03 09:32:29 -0300

    showing dish

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -27,6 +27,20 @@ class DishesController {
 
     response.json();
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const dish = await knex("dishes").where({ id }).first();
+    const ingredients = await knex("ingredients")
+      .where({ dish_id: id })
+      .orderBy("name");
+
+    return response.json({
+      ...dish,
+      ingredients,
+    });
+  }
 }
 
 module.exports = DishesController;
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,5 +7,6 @@ const dishesRoutes = Router();
 const dishesController = new DishesController();
 
 dishesRoutes.post("/:user_id", dishesController.create);
+dishesRoutes.get("/:id", dishesController.show);
 
 module.exports = dishesRoutes;
