0de84a2e905b481b1863e422c2ee8844d3f857d7
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-03-06 16:15:58 -0300

    adding authentication middleware

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex");
 class DishesController {
   async create(request, response) {
     const { name, description, category, price, image, ingredients } = request.body;
-    const { user_id } = request.params;
+    const user_id = request.user.id;
 
     const dish_id = await knex("dishes").insert({
       name,
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 
 const DishesController = require("../controllers/DishesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const dishesRoutes = Router();
 
 const dishesController = new DishesController();
 
+dishesRoutes.use(ensureAuthenticated);
+
 dishesRoutes.get("/", dishesController.index);
-dishesRoutes.post("/:user_id", dishesController.create);
+dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);
 dishesRoutes.patch("/:id", dishesController.update);
