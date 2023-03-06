247a9a2d90d50ea114fca134d96f271f8ebed816
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-03-06 12:09:48 -0300

    validating username and password

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,32 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
-    return response.json({ email, password });
+    const user = await knex("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorretos", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorretos", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
 
